7223d4f2cc94b05facd768ab3a66edecfd66c39e
AxelSantander26 <asantanderalcarraz@gmail.com>
2026-08-18 21:46:16 -0500

    test error simulate v2

--- src/com/example/openclose/OpenCloseApp.java
+++ src/com/example/openclose/OpenCloseApp.java
@@ -7,7 +7,7 @@ import com.example.openclose.descuentos.DescuentoVariable;
 public class OpenCloseApp {
     public static void main(String[] args) {
         // Definimo
-        double monto = hola;
+        String monto = hola;
         // Inicializamos un descuento variable
         Descuento descuento = new DescuentoVariable();
         // Aplicamos el primer descuento del 10%
